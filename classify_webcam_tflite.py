517a3dc3b39222801ba3599ad921b0dfa189e2e5
Hussein Shehata <66196821+hussein-shehata@users.noreply.github.com>
2023-02-13 22:50:19 +0200

    Initial Commit

---
+++ classify_webcam_tflite.py
@@ -0,0 +1,111 @@
+import sys
+import os
+
+# import matplotlib
+import numpy as np
+# import matplotlib.pyplot as plt
+import copy
+import cv2
+import tensorflow_hub as hub
+
+# Disable tensorflow compilation warnings
+os.environ['TF_CPP_MIN_LOG_LEVEL']='2'
+import tensorflow as tf
+
+
+interpreter = tf.lite.Interpreter(model_path='3_withoutlast9Layers_Model.tflite')
+interpreter.allocate_tensors()
+
+# Get input and output tensors.
+input_details = interpreter.get_input_details()
+output_details = interpreter.get_output_details()
+
+
+classes = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 
+           'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 
+           'W', 'X', 'Y', 'Z', 'del', 'nothing', 'space']
+
+def preprocess_Input(X):
+    np_X = np.array(X)
+    normalised_X = np_X.astype('float32')/255.0
+    normalised_X = np.expand_dims(normalised_X, axis=0)
+    return normalised_X
+
+
+def predict(image_data):
+
+    interpreter.set_tensor(input_details[0]['index'], image_data)
+    interpreter.invoke()
+
+    # Get the result 
+    output_data = interpreter.get_tensor(output_details[0]['index'])
+    Index = np.argmax(output_data)
+    max_score = output_data.max()
+    res = classes[Index]
+    return res, (max_score * 100)
+
+
+
+
+
+
+# Feed the image_data as input to the graph and get first prediction
+
+c = 0
+
+cap = cv2.VideoCapture(0)
+
+res, score = '', 0.0
+i = 0
+mem = ''
+consecutive = 0
+sequence = ''
+
+while True:
+    ret, img = cap.read()
+    img = cv2.flip(img, 1)
+    
+    if ret:
+        x1, y1, x2, y2 = 100, 100, 300, 300
+        img_cropped = img[y1:y2, x1:x2]
+        frame = cv2.resize(img_cropped, (200, 200), interpolation=cv2.INTER_CUBIC)
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        c += 1
+        # image_data = cv2.imencode('.jpg', frame)[1].tostring()
+        image_data = tf.keras.utils.img_to_array(frame)
+        image_data = preprocess_Input(image_data)
+        
+        a = cv2.waitKey(1) # waits to see if `esc` is pressed
+        
+        if i == 4:
+            res_tmp, score = predict(image_data)
+            res = res_tmp
+            i = 0
+            if mem == res:
+                consecutive += 1
+            else:
+                consecutive = 0
+            if consecutive == 2 and res not in ['nothing']:
+                if res == 'space':
+                    sequence += ' '
+                elif res == 'del':
+                    sequence = sequence[:-1]
+                else:
+                    sequence += res
+                consecutive = 0
+        i += 1
+        cv2.putText(img, '%s' % (res.upper()), (100,400), cv2.FONT_HERSHEY_SIMPLEX, 4, (255,255,255), 4)
+        cv2.putText(img, '(score = %.5f)' % (float(score)), (100,450), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,255))
+        mem = res
+        cv2.rectangle(img, (x1, y1), (x2, y2), (255,0,0), 2)
+        cv2.imshow("img", img)
+        img_sequence = np.zeros((200,1200,3), np.uint8)
+        cv2.putText(img_sequence, '%s' % (sequence.upper()), (30,30), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,255), 2)
+        cv2.imshow('sequence', img_sequence)
+        
+        if a == 27: # when `esc` is pressed
+            break
+
+# Following line should... <-- This should work fine now
+cv2.destroyAllWindows() 
+cv2.VideoCapture(0).release()
